b5e4e66035d7b5ceb2e4abdde72262936bd454ff
apip <apip@gitlab.lab>
2020-02-02 16:03:09 +0700

    membuat constructor function

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let mahasiswa2 = {
 
 /* Function Declaration */
 function Mahasiswa(nama, energi) {
-    let mahasiswa = {};
+    let mahasiswa = {}; //dekralasi variable
     mahasiswa.nama = nama;
     mahasiswa.energi = energi;
 
@@ -41,7 +41,24 @@ let apip = Mahasiswa('apip', 10);
 let aah = Mahasiswa('aah', 5);
 
 
-/* Constructor Function */
+/* Constructor Function */ // keyword New
+function MahasiswaBaru(nama, energi) {
+    this.nama = nama;
+    this.energi = energi;
+
+    mahasiswa.makan = function (porsi) {
+        this.energi += porsi;
+        console.log('halo ' + this.nama + ', Selamat Makan!');
+    }
+
+    mahasiswa.main = function (jam) {
+        this.energi -= jam;
+        console.log('halo ' + this.nama + ', Selamat Bermain!');
+    }
+}
+
+let apipK = new MahasiswaBaru('apip', 10);
+let aahS = new MahasiswaBaru('aah', 5);
 
 
 /* Object.Create */
