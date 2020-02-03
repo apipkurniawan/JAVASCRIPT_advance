d33cddf29b75adfcb940503364f629c9490ec622
apip <apip@gitlab.lab>
2020-02-03 11:31:49 +0700

    contoh higher oreder funct

--- lat_higher_order_function.js
+++ lat_higher_order_function.js
@@ -1,12 +1,17 @@
 /* kerjakanTugas adalah Higher Order Function */
 /* selesai adalah callback */
-function kerjakanTugas(mataKuliah, selesai) {
-    console.log('Mulai mengerjakan tugas ' + mataKuliah + ' ....');
-    selesai();
-}
+// function kerjakanTugas(mataKuliah, selesai) {
+//     console.log('Mulai mengerjakan tugas ' + mataKuliah + ' ....');
+//     selesai();
+// }
 
-function selesai() {
-    alert('Selesai mengerjakan tugas!');
-}
+// function selesai() {
+//     alert('Selesai mengerjakan tugas!');
+// }
+// kerjakanTugas('Pemrograman Web', selesai);
 
-kerjakanTugas('Pemrograman Web', selesai);
+
+
+/* contoh higher order function */
+/* Array.prototype.map() */
+// blm
